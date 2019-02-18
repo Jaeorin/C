017151d89442202df0e2ee6eab631bbc38976f31
lpmarz3 <46807050+lpmarz3@users.noreply.github.com>
2019-02-18 13:22:42 +0900

    파일 정리
    어디서 C/C++ 배웠다 하지 말 것...
    다른 소스파일인데도 main 함수 중복이 되는 이유를 찾아야 함

--- src/study/hello_190214.c
+++ src/study/hello_190214.c
@@ -1,14 +1,17 @@
-/*
- * hello.c
- *
- *  Created on: 2019. 2. 14.
- *      Author: it
- */
 
 #include <stdio.h>
 
 int main() {
-	printf("Hello C");
-	return 0;
-}
 
+	// 190214
+	printf("Hello C \n");
+
+	// 181224
+	int a;
+	for(a = 1; a < 10; a++){
+		printf("3 * %d = %d \n", a, a * 3);
+	}
+
+	return 0;
+
+}
